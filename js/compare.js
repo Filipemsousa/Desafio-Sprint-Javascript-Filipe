932c85174a7b2e204f4778d961c772993ff37ffa
Filipemsousa <filipmenzessousa@gmail.com>
2025-10-14 16:25:38 -0300

    finalizacao da comparacao

--- js/compare.js
+++ js/compare.js
@@ -68,7 +68,35 @@ function UpdateCompareTable() {
     const car2 = carArr[1];
     document.querySelector("#compare_image_0").innerHTML = `<img src="${car1.image}" alt="">`;
     document.querySelector("#compare_image_1").innerHTML = `<img src="${car2.image}" alt="">`;
+    
     document.querySelector("#compare_modelo_0").innerHTML = `<p>${car1.nome}</p>`;
     document.querySelector("#compare_modelo_1").innerHTML = `<p>${car2.nome}</p>`;
     
+    document.querySelector("#compare_alturacacamba_0").innerHTML = `<p>${car1.alturaCacamba}</p>`;
+    document.querySelector("#compare_alturacacamba_1").innerHTML = `<p>${car2.alturaCacamba}</p>`;
+
+    document.querySelector("#compare_alturaveiculo_0").innerHTML = `<p>${car1.alturaVeiculo}</p>`;
+    document.querySelector("#compare_alturaveiculo_1").innerHTML = `<p>${car2.alturaVeiculo}</p>`;
+
+    document.querySelector("#compare_alturasolo_0").innerHTML = `<p>${car1.alturaSolo}</p>`;
+    document.querySelector("#compare_alturasolo_1").innerHTML = `<p>${car2.alturaSolo}</p>`;
+
+    document.querySelector("#compare_capacidadecarga_0").innerHTML = `<p>${car1.capacidadeCarga}</p>`;
+    document.querySelector("#compare_capacidadecarga_1").innerHTML = `<p>${car2.capacidadeCarga}</p>`;
+
+    document.querySelector("#compare_motor_0").innerHTML = `<p>${car1.motor}</p>`;
+    document.querySelector("#compare_motor_1").innerHTML = `<p>${car2.motor}</p>`;
+
+    document.querySelector("#compare_potencia_0").innerHTML = `<p>${car1.potencia}</p>`;
+    document.querySelector("#compare_potencia_1").innerHTML = `<p>${car2.potencia}</p>`;
+
+    document.querySelector("#compare_volumecacamba_0").innerHTML = `<p>${car1.volumeCacamba}</p>`;
+    document.querySelector("#compare_volumecacamba_1").innerHTML = `<p>${car2.volumeCacamba}</p>`;
+
+    document.querySelector("#compare_roda_0").innerHTML = `<p>${car1.roda}</p>`;
+    document.querySelector("#compare_roda_1").innerHTML = `<p>${car2.roda}</p>`;
+
+    document.querySelector("#compare_preco_0").innerHTML = `<p>${car1.preco}</p>`;
+    document.querySelector("#compare_preco_1").innerHTML = `<p>${car2.preco}</p>`;
+
 }
