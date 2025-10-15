626a5c5d478784ee79b40b247ca24e6d3e4f20a0
Filipemsousa <filipmenzessousa@gmail.com>
2025-10-14 21:18:30 -0300

    inicio do armazenamento

--- js/form.js
+++ js/form.js
@@ -1,8 +1,16 @@
+const contatoArr = [];
 
 //class contato
 
 class contato {
-    
+    constructor(nome,sobrenome,email,cpf,telefone,contato){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.contato = contato;
+    }
 }
 
 function Post(form) {
@@ -13,15 +21,29 @@ function Post(form) {
             form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
             form.elements.namedItem("contato").value);
-  
+    
+              // LINHAS DE CONFIRMAÇÃO CRUCIAIS:
+            console.log("--- NOVO CONTATO ARMAZENADO ---");
+            console.log(data); // Exibe o objeto recém-criado
+            console.log("Total na Agenda:", agenda.length); // Exibe a contagem atual
+            console.log("--------------------------------");
+    
+            contatoArr.push(data);
+            return data; // Retorna o objeto criado
+
 }
 
-function Enviar() {
+function Enviar(event) {
 
-    var nome = document.getElementById("nomeid");
+    if (event && event.preventDefault) {
+        event.preventDefault(); 
+    }
 
-    if (nome.value != "") {
+    //var nome = document.getElementById("nomeid");
+
+    if (nome.value != "" && sobrenome.value !="" && email.value != "" && cpf.value != "" && telefone.value != "" && contato.value != "") {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+        
     }
 
 }
